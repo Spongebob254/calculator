17266b728f4d15654baffdae5a7dcdeac9d0cc29
Spongebob254 <faymwango1@gmail.com>
2020-05-30 10:29:43 +0300

    business logic fnx of add,subtract,divide and multiply added
    git commit -m business logic fnx of add,subtract,divide and multiply added

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+var add = function(number1, number2) {
+    return number1 + number2;
+  };
+  
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+  var result = add(number1, number2);
+  alert(result);
+  //subtraction fxn
+  var subtract=function (number1,number2){
+      return number1-number2;
+  };
+  var number1=parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+  var result=subtract(number1,number2);
+  alert(result);
+  //divide fxn
+  var divide=function (number1,number2){
+      return number1/number2;
+  };
+ var number1=parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+  var result=divide (number1,number2);
+  alert(result);
+  //multipy function
+ 
+  var multipy=function (number1,number2){
+    return number1*number2;
+};
+var number1=parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result=multipy(number1,number2);
+alert(result);
